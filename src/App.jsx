34b6aaa1c206d24a039fbc103c6cfc6a3fa12d14
Arman <armanfaruque786@gmail.com>
2024-08-08 23:24:19 +0530

    imbd

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./Components/Navbar"
 import Watchlist from "./Components/Watchlist"
 import { Routes,Route } from "react-router-dom"
 import './App.css'
+import WatchListContextWrapper from "./Context/WathclistContext"
 
 function App() {
  
@@ -11,12 +12,12 @@ function App() {
   return (
     <Fragment>
       <Navbar/>
-
+     <WatchListContextWrapper>
     <Routes>
       <Route path='/' element = {<Home></Home>}/>
       <Route path='/watchlist' element = {<Watchlist></Watchlist>}/>
     </Routes>
-
+    </WatchListContextWrapper>
     </Fragment>
    )
 }
